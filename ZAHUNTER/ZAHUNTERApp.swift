9707d178d713310caf99f3c8335a84f390031d07
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-04-01 20:11:34 -0500

    Initial Commit

---
+++ ZAHUNTER/ZAHUNTERApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZAHUNTERApp.swift
+//  ZAHUNTER
+//
+//  Created by Nicholas Papierniak on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ZAHUNTERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
