70f36cf6afa91f7e4ac45f7a87575e0a92adb193
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-04-22 09:26:18 -0500

    ahhhh

--- ZAHUNTER/LocationManger.swift
+++ ZAHUNTER/LocationManger.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
